bacfa63010a8a43505025b346b92eb6719b29fec
Clark Moody <clark@clarkmoody.com>
2021-06-14 21:24:27 -0500

    Rename random word module from `freq` to `random`

--- src/data.rs
+++ src/data.rs
@@ -1,10 +1,10 @@
-pub mod freq;
 pub mod keyboard;
 pub mod profile;
+pub mod random;
 pub mod style;
 pub mod theme;
 pub mod training;
 
-pub use freq::Freq;
+pub use random::Freq;
 pub use profile::User;
 pub use theme::Theme;
